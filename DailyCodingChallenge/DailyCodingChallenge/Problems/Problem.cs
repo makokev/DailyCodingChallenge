d355702c685a0db74d815b03697020f9718ed425
Kevin Leto <kevinleto1995@gmail.com>
2019-01-09 16:56:07 +0100

    Main created. Problem base structure created.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// A generic problem. It contains its number and its description.
+public abstract class Problem
+{
+	#region Static region
+
+	// Dictionary containing pairs (problem_number, problem_type) used for dinamically initialization.
+	private static Dictionary<int, Type> _problems;
+
+	// Creating and initialising the dictionary
+	static Problem()
+	{
+		Problem problem;
+		_problems = new Dictionary<int, Type>();
+		
+		// Using reflection to extract from the assembies all the subtypes of the class "Problem"
+		var types = from assembly in AppDomain.CurrentDomain.GetAssemblies()
+					from type in assembly.GetTypes()
+					where type.IsSubclassOf(typeof(Problem)) && !type.IsAbstract
+					select type;
+		foreach (Type t in types)
+		{
+			problem = (Problem)Activator.CreateInstance(t);
+			_problems.Add(problem.ProblemNumber, t);
+		}
+	}
+
+	public static List<Type> GetProblemTypes() => _problems.Values.ToList();
+
+	public static Problem GetProblem(int problemNumber)
+	{
+		Problem problem = null;
+		if (_problems.ContainsKey(problemNumber))
+			problem = (Problem)Activator.CreateInstance(_problems[problemNumber]);
+
+		return problem;
+	}
+
+	#endregion
+
+	#region Private members
+
+	private int _problemNumber;
+	private string _description;
+
+	#endregion
+
+	#region Constructor
+
+	protected Problem(int problemNumber, string description)
+	{
+		if (problemNumber <= 0)
+			new ArgumentOutOfRangeException("Not-positive numbers are not ammitted!");
+		if (null == description)
+			new ArgumentNullException("Null description is not ammitted!");
+		_problemNumber = problemNumber;
+		_description = description;
+	}
+
+	#endregion
+
+	#region Public methods and Properties
+
+	public override string ToString() => "Problem" + _problemNumber;
+
+	public string PrintDescription => _description;
+
+	public int ProblemNumber => _problemNumber;
+
+	#endregion
+
+	#region Abstract method
+
+	public abstract void run();
+
+	#endregion
+}
--- DailyCodingChallenge/DailyCodingChallenge/Program.cs
+++ DailyCodingChallenge/DailyCodingChallenge/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DailyCodingChallenge
 {
@@ -10,8 +8,55 @@ namespace DailyCodingChallenge
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World");
-			Console.ReadLine();
+			bool continueWhile = true;
+			while (continueWhile)
+			{
+				bool parsingSuccessed = false;
+				int problemNumber = 1;
+				Console.WriteLine("List of problems:");
+
+				// Printing problem's names
+				foreach(Type t in Problem.GetProblemTypes())
+					Console.WriteLine(t.ToString().Split('.')[2]);
+
+				Console.WriteLine();
+
+				// Parsing problem number
+				while (!parsingSuccessed)
+				{
+					Console.Write("Insert the number of the problem that you would resolve (to exit: 0):");
+					try
+					{
+						problemNumber = Convert.ToInt32(Console.ReadLine());
+						parsingSuccessed = true;
+					}
+					catch (Exception)
+					{
+						Console.WriteLine("Number parsing error. Retry.");
+					}
+				}
+				if (0 != problemNumber)
+				{
+					Problem p = Problem.GetProblem(problemNumber);
+					Console.WriteLine();
+
+					// If the problem exists, run the implementation
+					if (null != p)
+						p.run();
+					else
+						Console.WriteLine("Problem " + problemNumber + " doesn't exist.");
+					Console.WriteLine("\nEnd Problem.\n");
+					Console.WriteLine("------------------------------------------------------------");
+				}
+				else
+				{
+					Console.WriteLine("Exit.");
+					continueWhile = false;
+				}
+			}
+
+			Console.Read();
 		}
 	}
+	
 }

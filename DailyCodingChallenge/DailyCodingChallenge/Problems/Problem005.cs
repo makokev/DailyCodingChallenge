36f80940c3e5aa157b7684383b1f011b70101825
Kevin Leto <kevinleto1995@gmail.com>
2019-01-09 17:51:07 +0100

    Problem #5 solved.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem005.cs
@@ -0,0 +1,25 @@
+﻿using DailyCodingChallenge.Problems.Utility;
+using System;
+
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem005 : Problem
+	{	
+		public Problem005() : base(5, ProblemDescription.PROBLEM005_DESCRIPTION) { }
+
+		public override void run()
+		{
+			Console.WriteLine(this.ToString());
+			Console.WriteLine(this.PrintDescription() + "\n");
+			Pair<int, int> pair = new Pair<int, int>(3, 4);
+			Console.WriteLine("Input: Pair = " + pair.ToString());
+			Console.WriteLine("Trying car(cons(3,4)) = 3 ...");
+			Console.Write("Result: ");
+			if (Pair<int,int>.car(Pair<int, int>.cons(3, 4)) == 3)
+				Console.WriteLine("Success!");
+			else
+				Console.WriteLine("Wrong!");
+		}
+	}
+}
--- DailyCodingChallenge/DailyCodingChallenge/Problems/ProblemDescription.cs
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/ProblemDescription.cs
@@ -8,6 +8,8 @@ namespace DailyCodingChallenge.Problems
 		public static string PROBLEM002_DESCRIPTION = "Given an array of integers, return a new array such that\neach element at index i of the new array is the product of all the numbers in the original array\nexcept the one at index i.\nFor example, given the array [1, 2, 3, 4, 5] the output will be [120, 60, 40, 30, 24].\nBonus: what if you can't use division?";
 		public static string PROBLEM003_DESCRIPTION = "Given the root to a binary tree, implement serialize(root),\nwhich serializes the tree into a string, and deserialize(s), which deserializes the string back\ninto the tree.\nThe following test should pass: node = Node('root', Node('left', Node('left.left')), Node('right'))\nassert deserialize(serialize(node)).left.left.val == 'left.left'";
 		public static string PROBLEM004_DESCRIPTION = "Given an array of integers, find the first missing positive integer in linear time and constant space.\nIn other words, find the lowest positive integer that does not exist in the array.\nThe array can contain duplicates and negative numbers as well.\nFor example, the input[3, 4, -1, 1] should give 2. The input[1, 2, 0] should give 3.\nYou can modify the input array in-place.";
+		public static string PROBLEM005_DESCRIPTION = "cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.\nFor example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.\nImplement car and cdr.";
+
 
 	}
 }

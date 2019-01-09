2b706aa57456b626da7c226cbec0b92e05460e5d
Kevin Leto <kevinleto1995@gmail.com>
2019-01-09 17:47:32 +0100

    Problem 4# solved. ProblemDescription updated.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem004.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem004 : Problem
+	{
+		#region Input
+
+		private int[] values = new int[] { 3, 4, -1, 1, 2, 6 };
+
+		#endregion
+
+		public Problem004() : base(4, ProblemDescription.PROBLEM004_DESCRIPTION){ }
+
+		public override void run()
+		{
+			Console.WriteLine(this.ToString());
+			Console.WriteLine(this.PrintDescription() + "\n");
+
+			List<int> list = values.ToList();
+			Console.Write("Input: [");
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (0 == i)
+					Console.Write(list[i]);
+				else
+					Console.Write(", " + list[i]);
+			}
+			Console.WriteLine("]");
+
+			int minMissing = (list.Count > 0) ? 1 : int.MinValue;
+
+			while (list.Count > 0)
+			{
+				int actualMin = list.Min();
+				if (minMissing == actualMin)
+					minMissing++;
+				else if (actualMin > minMissing)
+					break;
+				list.Remove(actualMin);
+			}
+
+			if (minMissing != int.MinValue)
+				Console.WriteLine("Minimum missing integer value: " + minMissing);
+			else
+				Console.WriteLine("The list is empty.");
+
+		}
+	}
+}
--- DailyCodingChallenge/DailyCodingChallenge/Problems/ProblemDescription.cs
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/ProblemDescription.cs
@@ -4,23 +4,10 @@ namespace DailyCodingChallenge.Problems
 	// This calss contains all the problem's descriptions
 	class ProblemDescription
 	{
-		public static string PROBLEM001_DESCRIPTION = "Given a list of numbers and a number k, " +
-			"return whether any two numbers from the list add up to k.\n" +
-			"For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 = 17.\n" +
-			"Bonus: Can you do this in one pass?";
-
-		public static string PROBLEM002_DESCRIPTION = "Given an array of integers, return a new array such that\n" +
-			"each element at index i of the new array is the product of all the numbers in the original array\n" +
-			"except the one at index i.\n" +
-			"For example, given the array [1, 2, 3, 4, 5] the output will be [120, 60, 40, 30, 24].\n" +
-			"Bonus: what if you can't use division?";
-
-		public static string PROBLEM003_DESCRIPTION = "Given the root to a binary tree, implement serialize(root),\n" +
-			"which serializes the tree into a string, and deserialize(s), which deserializes the string back\n" +
-			"into the tree.\n" +
-			"The following test should pass: node = Node('root', Node('left', Node('left.left')), Node('right'))\n" +
-			"assert deserialize(serialize(node)).left.left.val == 'left.left'";
-
+		public static string PROBLEM001_DESCRIPTION = "Given a list of numbers and a number k, return whether any two numbers from the list add up to k.\nFor example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 = 17.\nBonus: Can you do this in one pass?";
+		public static string PROBLEM002_DESCRIPTION = "Given an array of integers, return a new array such that\neach element at index i of the new array is the product of all the numbers in the original array\nexcept the one at index i.\nFor example, given the array [1, 2, 3, 4, 5] the output will be [120, 60, 40, 30, 24].\nBonus: what if you can't use division?";
+		public static string PROBLEM003_DESCRIPTION = "Given the root to a binary tree, implement serialize(root),\nwhich serializes the tree into a string, and deserialize(s), which deserializes the string back\ninto the tree.\nThe following test should pass: node = Node('root', Node('left', Node('left.left')), Node('right'))\nassert deserialize(serialize(node)).left.left.val == 'left.left'";
+		public static string PROBLEM004_DESCRIPTION = "Given an array of integers, find the first missing positive integer in linear time and constant space.\nIn other words, find the lowest positive integer that does not exist in the array.\nThe array can contain duplicates and negative numbers as well.\nFor example, the input[3, 4, -1, 1] should give 2. The input[1, 2, 0] should give 3.\nYou can modify the input array in-place.";
 
 	}
 }

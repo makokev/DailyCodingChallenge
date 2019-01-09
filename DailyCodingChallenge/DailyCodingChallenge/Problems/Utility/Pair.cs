b1b0ce0e423f0c743296f74d9161892f5c62294d
Kevin Leto <kevinleto1995@gmail.com>
2019-01-09 18:25:12 +0100

    Problem #5 auxiliar class.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Utility/Pair.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems.Utility
+{
+	class Pair<T, W>
+	{
+
+		public Pair(T a, W b)
+		{
+			this.a = a;
+			this.b = b;
+		}
+
+		public T a { get; }
+		public W b { get; }
+
+		public override string ToString()
+		{
+			return "(" + a + ", " + b + ")";
+		}
+
+		public static Pair<T, W> cons(T a, W b)
+		{
+			return new Pair<T, W>(a, b);
+		}
+
+		public static T car(Pair<T, W> pair)
+		{
+			return pair.a;
+		}
+
+		public static W cdr(Pair<T, W> pair)
+		{
+			return pair.b;
+		}
+	}
+}

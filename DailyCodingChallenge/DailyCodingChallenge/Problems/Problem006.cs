e9ff6194cc5d1dc7988b60cdc3cdebe0f9e27d2d
Kevin Leto <kevinleto1995@gmail.com>
2019-01-10 01:14:38 +0100

    Problem #6 solved.

--- DailyCodingChallenge/DailyCodingChallenge/Problems/Problem006.cs
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem006.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using DailyCodingChallenge.Problems.Utility;
+using System;
+using System.Collections.Generic;
 
 namespace DailyCodingChallenge.Problems
 {
+	// C = A ^ B = B ^ A
+	// A = C ^ B = B ^ C
+	// B = C ^ A = A ^ C
+
 	class Problem006 : Problem
 	{
 		public Problem006() : base(6, ProblemDescription.PROBLEM006_DESCRIPTION) { }
@@ -10,8 +16,30 @@ namespace DailyCodingChallenge.Problems
 		{
 			Console.WriteLine(this.ToString());
 			Console.WriteLine(this.PrintDescription() + "\n");
-
-			Console.WriteLine("No implementation jet.");
+			XORLinkedList<int> list = new XORLinkedList<int>();
+			list.Add(1);
+			list.Add(2);
+			list.Add(3);
+			Console.WriteLine("List:");
+			Console.WriteLine("list[0] = " + list.Get(0));
+			Console.WriteLine("list[1] = " + list.Get(1));
+			Console.WriteLine("list[2] = " + list.Get(2));
+			list.Remove(0);
+			list.Add(4);
+			Console.WriteLine("List:");
+			Console.WriteLine("list[0] = " + list.Get(0));
+			Console.WriteLine("list[1] = " + list.Get(1));
+			Console.WriteLine("list[2] = " + list.Get(2));
+			list.Remove(1);
+			Console.WriteLine("List:");
+			Console.WriteLine("list[0] = " + list.Get(0));
+			Console.WriteLine("list[1] = " + list.Get(1));
+			list.Add(2);
+			list.Remove(2);
+			list.Remove(1);
+			Console.WriteLine("List:");
+			Console.WriteLine("list[0] = " + list.Get(0));
+			
 		}
-	}
+	}	
 }
---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Utility/XORLinkedList.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DailyCodingChallenge.Problems.Utility
+{
+	class XORLinkedList<ValueType>
+	{
+		private readonly Dictionary<int, XORNode> memory = new Dictionary<int, XORNode>();
+		private int memoryAddress;
+		private int _count;
+
+		private int _first;
+		private int _last;
+
+		public XORLinkedList()
+		{
+			memory.Add(0, new XORNode { Address = 0, Both = 0 ^ 0 });
+			memoryAddress = 1;
+			_count = 0;
+			_first = 0;
+			_last = _first;
+		}
+
+		public int Count => _count;
+
+		public void Add(ValueType value)
+		{
+			if (memory[_first].Address == 0)
+			{
+				memory.Add(memoryAddress, new XORNode { Address = memoryAddress, Both = 0 ^ 0, Value = value });
+				_first = memoryAddress;
+				_last = _first;
+			}
+			else
+			{
+				XORNode secondLast = memory[_last];
+				XORNode node = new XORNode { Address = memoryAddress, Both = 0 ^ secondLast.Address, Value = value };
+				secondLast.Both = secondLast.Both ^ node.Address;
+				_last = memoryAddress;
+				memory.Add(memoryAddress, node);
+			}
+			_count++;
+			memoryAddress++;
+		}
+
+		public ValueType Get(int index)
+		{
+			if (index >= Count)
+				new ArgumentOutOfRangeException("Index out of bound exception.");
+
+			XORNode currentNode = memory[_first];
+			XORNode previewsNode = memory[0];
+			int address;
+			for (int i = 0; i < index; i++)
+			{
+				address = previewsNode.Address ^ currentNode.Both;
+				previewsNode = currentNode;
+				currentNode = memory[address];
+			}
+			return currentNode.Value;
+		}
+
+		public void Remove(int index)
+		{
+			XORNode currentNode, previousNode, nextNode;
+			int address;
+			if (index >= Count)
+				new ArgumentOutOfRangeException("Index out of bound exception.");
+
+			currentNode = memory[_first];
+			previousNode = memory[0];
+			if (index == 0) // remove the first node
+			{
+				nextNode = memory[currentNode.Both];
+				if (Count == 1) // only one node
+				{
+					memory.Remove(_first);
+					_first = 0;
+					_last = 0;
+				}
+				else // more than one nodes
+				{
+					XORNode node = memory[_first];
+					memory.Remove(_first);
+					nextNode = memory[node.Both];
+					nextNode.Both = nextNode.Both ^ _first;
+					_first = nextNode.Address;
+				}
+			}
+			else
+			{
+				// obtain the node that must be eliminated (currentNode)
+				for (int i = 0; i < index; i++)
+				{
+					address = previousNode.Address ^ currentNode.Both;
+					previousNode = currentNode;
+					currentNode = memory[address];
+				}
+
+				if (currentNode.Address == _last) // remove the last node
+				{
+					int previousAddress = previousNode.Both ^ currentNode.Address;
+					previousNode.Both = previousAddress;
+					_last = previousNode.Address;
+				}
+				else // remove another node
+				{
+					nextNode = memory[currentNode.Both ^ previousNode.Address];
+					int nextAddress = nextNode.Both ^ currentNode.Address;
+					nextNode.Both = nextAddress ^ previousNode.Address;
+					int previousAddress = previousNode.Both ^ currentNode.Address;
+					previousNode.Both = previousAddress ^ nextNode.Address;
+				}
+			}
+			_count--;
+		}
+
+		class XORNode
+		{
+			public int Address { get; set; }
+			public int Both { get; set; }
+			public ValueType Value { get; set; }
+
+			public XORNode() { }
+
+		}
+
+	}
+}

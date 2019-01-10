a99e88dbe71fbb335a0479f6975b4c41addb8923
Kevin Leto <kevinleto1995@gmail.com>
2019-01-10 16:37:02 +0100

    Problem #11 solved.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem011.cs
@@ -0,0 +1,52 @@
+﻿using DailyCodingChallenge.Problems.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem011 : Problem
+	{
+		#region Static region
+
+		private static List<string> WORDS;
+
+		static Problem011()
+		{
+			WORDS = new List<string>();
+			WORDS.Add("ciao");
+			WORDS.Add("ciaone");
+			WORDS.Add("help");
+			WORDS.Add("help-desk");
+			WORDS.Add("macho");
+		}
+
+		#endregion
+
+		public Problem011() : base(11, ProblemDescription.PROBLEM011_DESCRIPTION) { }
+
+		public override void run()
+		{
+			Console.WriteLine(this.ToString());
+			Console.WriteLine(this.PrintDescription() + "\n");
+			AutocompleteSystem autocompleteSystem = new AutocompleteSystem();
+			WORDS.ForEach(autocompleteSystem.Add);
+
+			Console.Write("Insert the prefix: ");
+			string prefix = Console.ReadLine();
+			
+			Console.WriteLine("\nPrefix readed: '" + prefix + "'.\n");
+			
+			List<string> words = autocompleteSystem.FindWordByPrefix(prefix);
+			if (0 == words.Count)
+				Console.WriteLine("No words founded.");
+			else
+				Console.WriteLine("Words founded:");
+			words.ForEach(Console.WriteLine);
+			
+		}
+
+	}
+}
---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Utility/AutocompleteSystem.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems.Utility
+{
+	class AutocompleteSystem
+	{
+		private AutocompleteNode _root;
+
+		public AutocompleteSystem()
+		{
+			_root = new AutocompleteNode('\0', "");
+		}
+
+		public void Add(string word)
+		{
+			AutocompleteNode currentNode = _root;
+			foreach(char c in word)
+			{
+				if (currentNode.Children.ContainsKey(c))
+				{
+					currentNode = currentNode.Children[c];
+				}
+				else
+				{
+					currentNode.Children.Add(c, new AutocompleteNode(c, currentNode.Word + c));
+					currentNode = currentNode.Children[c];
+				}
+			}
+			if(!currentNode.Children.ContainsKey('\0'))
+				currentNode.Children.Add('\0', new AutocompleteNode('\0', currentNode.Word));
+		}
+
+		public List<string> FindWordByPrefix(string prefix)
+		{
+			List<string> list = new List<string>();
+			AutocompleteNode currentNode = _root;
+			bool prefixFounded = true;
+			foreach(char c in prefix)
+			{
+				if (currentNode.Children.ContainsKey(c))
+					currentNode = currentNode.Children[c];
+				else
+					prefixFounded = false; ;
+			}
+			if (prefixFounded)
+			{
+				list.AddRange(GetWords(currentNode));
+			}
+			return list;
+		}
+
+		private List<string> GetWords(AutocompleteNode root)
+		{
+			List<string> list = new List<string>();
+			AutocompleteNode currentNode = root;
+			foreach(AutocompleteNode node in currentNode.Children.Values)
+			{
+				if (node.IsLeaf)
+					list.Add(node.Word);
+				else
+					list.AddRange(GetWords(node));
+			}
+			return list;
+		}
+
+	}
+
+	class AutocompleteNode
+	{
+		private char _letter;
+		private string _word;
+		private readonly Dictionary<char, AutocompleteNode> _children;
+
+		public AutocompleteNode(char letter, string word)
+		{
+			_letter = letter;
+			_word = word;
+			_children = new Dictionary<char, AutocompleteNode>();
+		}
+
+		public Dictionary<char, AutocompleteNode> Children => _children;
+		public string Word => _word;
+		public char Letter => _letter;
+		public bool IsLeaf => _letter == '\0';
+	}
+}

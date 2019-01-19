9ff4c49ef3409327f457a4d09112e79d5a291746
Kevin Leto <kevinleto1995@gmail.com>
2019-01-19 16:24:33 +0100

    Refactoring: Problem #19.

--- DailyCodingChallenge/DailyCodingChallenge/Problems/Problem019.cs
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem019.cs
@@ -1,52 +1,42 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using DailyCodingChallenge.Problems.Utility;
 namespace DailyCodingChallenge.Problems
 {
 	class Problem019 : Problem
 	{
-		private static int N = 2;
+		private static int N = 3;
 		private static int K = 3;
 		private static int[] COLORS = new int[] { 0, 1, 2 };
-		
-
-		private static int[,] COSTS = new int[,] { { 1, 2, 2 }, { 2, 1, 1 }, { 2, 2, 1 } };
+		private static int[,] COSTS = new int[,] { { 2, 1, 2 }, { 2, 1, 1 }, { 2, 1, 1 } };
 
 		public Problem019() : base(19, ProblemDescription.PROBLEM019_DESCRIPTION) { }
 
 		protected override void Run()
 		{
-			List<int> colorList = COLORS.ToList();
-			int cost;
-			cost = GetMinCost(colorList);
+			int cost = GetMinCost();
 			Console.WriteLine("Minimum cost = " + cost);
-			
 		}
 
-		private int GetMinCost(List<int> colors)
+		private int GetMinCost()
 		{
-			return GetRecoursiveMinCost(0, 0, colors);
-			
+			return GetRecoursiveMinCost(0, 0, -1);
 		}
 
-		private int GetRecoursiveMinCost(int house, int partialCost, List<int> colors)
+		private int GetRecoursiveMinCost(int house, int partialCost, int previousColor)
 		{
 			if (house == N)
 				return partialCost;
 			else
 			{
-				house++;
 				int minCost = int.MaxValue;
-				for(int i = 0; i < colors.Count; i++)
+				for(int i = 0; i < COLORS.Length; i++)
 				{
-					List<int> nextColors = colors.ToList();
-					nextColors.RemoveAt(i);
-					int cost = GetRecoursiveMinCost(house, partialCost + COSTS[house, i], nextColors);
-					if(cost < minCost)
+					if (COLORS[i] != previousColor)
 					{
-						minCost = cost;
+						int cost = GetRecoursiveMinCost(house + 1, partialCost + COSTS[house, COLORS[i]], COLORS[i]);
+						if (cost < minCost)
+							minCost = cost;
 					}
 				}
 				return minCost;

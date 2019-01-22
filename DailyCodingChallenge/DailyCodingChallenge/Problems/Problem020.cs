a12cfda093f18b6e403ac7f67abed77ad8a78a6f
Kevin Leto <kevinleto1995@gmail.com>
2019-01-22 01:14:02 +0100

    Problem #20 solved.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem020.cs
@@ -0,0 +1,76 @@
+﻿using DailyCodingChallenge.Problems.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem020 : Problem
+	{
+		public Problem020() : base(20, ProblemDescription.PROBLEM020_DESCRIPTION) {	}
+
+		protected override void Run()
+		{
+			Console.WriteLine("List A:");
+			List<int> a = ReadInputIntList(-1);
+			Console.WriteLine("List B:");
+			List<int> b = ReadInputIntList(-1);
+
+			Console.WriteLine("List A: " + a.AsString());
+			Console.WriteLine("List B: " + b.AsString());
+
+
+			List<int> visited = new List<int>();
+			int nodeValue = -1;
+			bool founded = false;
+			while (!founded && a.Count() > 0 && b.Count() > 0)
+			{
+				if (visited.Contains(a.First()))
+				{
+					nodeValue = a.First();
+					founded = true;
+				}
+				else
+				{
+					visited.Add(a.First());
+					a.RemoveAt(0);
+				}
+
+				if (visited.Contains(b.First()))
+				{
+					nodeValue = b.First();
+					founded = true;
+				}
+				else
+				{
+					visited.Add(b.First());
+					b.RemoveAt(0);
+				}
+			}
+
+			if (founded)
+				Console.WriteLine("Node founded! Value = " + nodeValue);
+			else
+				Console.Write("No node founded.");
+		}
+	
+		private List<int> ReadInputIntList(int exitValue = 0)
+		{
+			List<int> list = new List<int>();
+			while (true)
+			{
+				Console.Write("Insert an integer value (to exit: " + exitValue + "): ");
+				if (int.TryParse(Console.ReadLine(), out int value))
+				{
+					if (value != exitValue)
+						list.Add(value);
+					else
+						break;
+				}
+			}
+			return list;
+		}
+	}
+}

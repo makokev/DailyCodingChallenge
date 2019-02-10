6e7f485337e71c44374ddefa653f09f9f54e8a13
Kevin Leto <kevinleto1995@gmail.com>
2019-02-10 17:51:48 +0100

    Problem #40 partially solved. Problem #41 solved.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem040.cs
@@ -0,0 +1,72 @@
+﻿using DailyCodingChallenge.Problems.Utility;
+using DailyCodingChallenge.Problems.Utility.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem040 : Problem
+	{
+		protected override string Description => ProblemDescription.PROBLEM040_DESCRIPTION;
+		protected override int Number => 40;
+
+		protected override void Run()
+		{
+			List<int> numbers = new List<int> { 6, 1, 3, 3, 3, 6, 6 };
+			Console.WriteLine("Input vector: {0}.", numbers.Print());
+
+			Console.WriteLine("SOLVED IN:\n - Time : O(n) OK\n - Space: O(n) NOPE\n");
+
+			int n = FoundSingleValue2(numbers);
+			Console.WriteLine("Value founded: {0}.", n);
+		}
+
+		/// <summary>
+		/// Time : O(n^2). NOPE
+		/// Space: O(1). OK
+		/// </summary>
+		/// <param name="numbers">The values' list.</param>
+		/// <returns>The only not-repeated value.</returns>
+		private int FoundSingleValue(List<int> numbers)
+		{
+			int n = -1;
+			while (numbers.Count > 0)
+			{
+				n = numbers[0];
+				numbers.Remove(n);
+				if (numbers.Contains(n))
+				{
+					numbers.Remove(n);
+					numbers.Remove(n);
+				}
+				else
+					break;
+			}
+			return n;
+		}
+
+
+		/// <summary>
+		/// Time : O(n). OK
+		/// Space: O(n). NOPE
+		/// </summary>
+		/// <param name="numbers">The values' list.</param>
+		/// <returns>The only not-repeated value.</returns>
+		private int FoundSingleValue2(List<int> numbers)
+		{
+			List<int> nums = new List<int>();
+			HashSet<int> set = new HashSet<int>();
+			foreach(int n in numbers)
+			{
+				if (set.Add(n))
+					nums.Add(n);
+				else
+					nums.Remove(n);
+			}
+			return nums[0];
+		}
+	}
+}
---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem041.cs
@@ -0,0 +1,80 @@
+﻿using DailyCodingChallenge.Problems.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem041 : Problem
+	{
+		protected override string Description => ProblemDescription.PROBLEM041_DESCRIPTION;
+		protected override int Number => 41;
+
+		protected override void Run()
+		{
+			// ('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')
+			List<Couple<string>> couples = new List<Couple<string>> {	new Couple<string>("A","B"),
+																		new Couple<string>("A","C"),
+																		new Couple<string>("B","C"),
+																		new Couple<string>("C","A")
+																	};
+			string startingPoint = "A";
+
+			Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+			foreach(Couple<string> couple in couples)
+			{
+				if (dictionary.Keys.Contains(couple.X))
+					dictionary[couple.X].Add(couple.Y);
+				else
+					dictionary[couple.X] = new List<string> { couple.Y };
+			}
+
+			List<string> path = GetPath(dictionary, startingPoint);
+			if (null != path)
+				Console.WriteLine("Path: {0}.", path.Print(separator:" -> ", withBrackets: false));
+			else
+				Console.WriteLine("No path founded.");
+			
+		}
+
+		private List<string> GetPath(Dictionary<string, List<string>> dictionary, string start)
+		{
+			List<string> path = new List<string> { start };
+			return RecoursiveGetPath(dictionary, path);
+		}
+		
+		private List<string> RecoursiveGetPath(Dictionary<string, List<string>> dictionary, List<string> partialPath)
+		{
+			if (dictionary.Count == 0 || null == partialPath)
+				return partialPath;
+
+			string start = partialPath.Last();
+			if (dictionary.Keys.Contains(start))
+			{
+				foreach(string newStart in dictionary[start])
+				{
+					Dictionary<string, List<string>> dictionaryCopy = CopyDictionary(dictionary);
+					List<string> newPath = partialPath.ToList();
+					newPath.Add(newStart);
+					dictionaryCopy[start].Remove(newStart);
+					if (0 == dictionaryCopy[start].Count)
+						dictionaryCopy.Remove(start);
+					List<string> thePath = RecoursiveGetPath(dictionaryCopy, newPath);
+					if (null != thePath)
+						return thePath;
+				}
+			}
+			return null;
+		}
+
+		private Dictionary<string, List<string>> CopyDictionary(Dictionary<string, List<string>> dictionary)
+		{
+			Dictionary<string, List<string>> dictionaryCopy = new Dictionary<string, List<string>>();
+			foreach(string key in dictionary.Keys)
+				dictionaryCopy.Add(key, dictionary[key].ToList());
+			return dictionaryCopy;
+		}
+	}
+}

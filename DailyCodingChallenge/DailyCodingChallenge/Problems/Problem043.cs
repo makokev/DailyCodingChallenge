65504f3d90c04da76d4b62848624d2cce8daedd9
Kevin Leto <kevinleto1995@gmail.com>
2019-02-12 00:46:42 +0100

    Problem #43 solved.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem043.cs
@@ -0,0 +1,27 @@
+﻿using DailyCodingChallenge.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem043 : Problem
+	{
+		protected override string Description => ProblemDescription.PROBLEM043_DESCRIPTION;
+		protected override int Number => 43;
+
+		protected override void Run()
+		{
+			MaxStack<int> stack = new MaxStack<int>();
+			stack.Push(5);
+			stack.Push(3);
+			stack.Push(18);
+			Console.WriteLine("Assert max = 18: {0}.", stack.Max() == 18);
+			int value = stack.Pop();
+			Console.WriteLine("Assert pop = 18: {0}.", value == 18);
+			Console.WriteLine("Assert max = 5: {0}.", stack.Max() == 5);
+		}
+	}
+}
---
+++ DailyCodingChallenge/DailyCodingChallenge/Utility/MaxStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Utility
+{
+	class MaxStack<ValueType> where ValueType : IComparable<ValueType>
+	{
+		// Linked list is very efficient in insert operations, more than the Stack<T> class
+		// that sometimes needs to adjust its dimension with time: O(n).
+		private LinkedNode<ValueType> StackRoot { get; set; }
+		private LinkedNode<ValueType> StackMaxRoot { get; set; }
+
+		public MaxStack()
+		{
+			StackRoot = null;
+			StackMaxRoot = null;
+		}
+
+		/// <summary>
+		/// Insert the value on top of the stack.
+		/// Time: O(1).
+		/// </summary>
+		/// <param name="value"></param>
+		public void Push(ValueType value)
+		{
+			LinkedNode<ValueType> valueNode = new LinkedNode<ValueType>(value);
+			if (null == StackRoot)
+			{
+				StackRoot = valueNode;
+				StackMaxRoot = new LinkedNode<ValueType>(value);
+			}
+			else
+			{
+				// push nello stack
+				valueNode.Next = StackRoot;
+				StackRoot = valueNode;
+
+				// aggiornamento max
+				ValueType max = (value.CompareTo(StackMaxRoot.Value) >= 0) ? value : StackMaxRoot.Value;
+				LinkedNode<ValueType> maxNode = new LinkedNode<ValueType>(max);
+				maxNode.Next = StackMaxRoot;
+				StackMaxRoot = maxNode;
+			}
+		}
+
+
+		/// <summary>
+		/// Remove and return the first value in the stack (LIFO).
+		/// If the stack is empty, default value is returned.
+		/// Time: O(1).
+		/// </summary>
+		/// <returns>The first value in the stack, or default if it's empty</returns>
+		public ValueType Pop()
+		{
+			if (null == StackRoot)
+				return default(ValueType);
+			ValueType value = StackRoot.Value;
+			StackRoot = StackRoot.Next;
+			StackMaxRoot = StackMaxRoot.Next;
+			return value;
+		}
+
+		/// <summary>
+		/// Return the max value in the stack.
+		/// </summary>
+		/// <returns>The max value in the stack</returns>
+		public ValueType Max() => StackMaxRoot.Value;
+	}
+
+	
+}

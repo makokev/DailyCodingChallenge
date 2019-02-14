5fe384a9cd0ae0122686b19d749f8744bb08db29
Kevin Leto <kevinleto1995@gmail.com>
2019-02-14 18:44:37 +0100

    Problem #45 solved.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem045.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem045 : Problem
+	{
+		protected override string Description => ProblemDescription.PROBLEM045_DESCRIPTION;
+		protected override int Number => 45;
+
+		private static Random _random = new Random();
+
+		protected override void Run()
+		{
+			int[] count = new int[7];
+			for (int i = 0; i < 7; i++)
+				count[i] = 0;
+			for(int i = 0; i < 10000; i++)
+				count[Rand7()-1]++;
+
+			Console.WriteLine("Results:");
+			for (int i = 0; i < 7; i++)
+				Console.WriteLine("count[{0}] = {1}.", i+1, count[i]);
+		}
+
+		private int Rand7()
+		{
+			int r = 0;
+			for (int i = 0; i < 7; i++)
+				r += Rand5();
+			return r % 7 + 1;
+		}
+		
+		private int Rand5()
+		{
+			return _random.Next(5) + 1;
+		}
+	}
+}

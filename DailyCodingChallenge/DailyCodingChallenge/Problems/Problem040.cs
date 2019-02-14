abdcfb752fee0393fa37cadeabe249f09abb2653
Kevin Leto <kevinleto1995@gmail.com>
2019-02-14 20:51:40 +0100

    Problem #40 solved with correct time and space complexity.

--- DailyCodingChallenge/DailyCodingChallenge/Problems/Problem040.cs
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem040.cs
@@ -15,58 +15,38 @@ namespace DailyCodingChallenge.Problems
 
 		protected override void Run()
 		{
-			List<int> numbers = new List<int> { 6, 1, 3, 3, 3, 6, 6 };
+			int[] numbers = new int[] { 6, 1, 3, 3, 3, 6, 6 };
 			Console.WriteLine("Input vector: {0}.", numbers.Print());
-
-			Console.WriteLine("SOLVED IN:\n - Time : O(n) OK\n - Space: O(n) NOPE\n");
-
-			int n = FoundSingleValue2(numbers);
+			
+			int n = FindSingleValue(numbers);
 			Console.WriteLine("Value founded: {0}.", n);
 		}
 
 		/// <summary>
-		/// Time : O(n^2). NOPE
-		/// Space: O(1). OK
+		/// Find the single value present in the array (Time: O(n) - Space: O(1)).
 		/// </summary>
-		/// <param name="numbers">The values' list.</param>
+		/// <param name="numbers">The values' array.</param>
 		/// <returns>The only not-repeated value.</returns>
-		private int FoundSingleValue(List<int> numbers)
+		private int FindSingleValue(int[] numbers)
 		{
-			int n = -1;
-			while (numbers.Count > 0)
-			{
-				n = numbers[0];
-				numbers.Remove(n);
-				if (numbers.Contains(n))
-				{
-					numbers.Remove(n);
-					numbers.Remove(n);
-				}
-				else
-					break;
-			}
-			return n;
-		}
+			int ones = 0, twos = 0, common_bits;
 
+			// Remind:
+			// & : and bit operator
+			// | : or  bit operator
+			// ^ : xor bit operator
+			// ~ : complement bit operator (bit inversion)
 
-		/// <summary>
-		/// Time : O(n). OK
-		/// Space: O(n). NOPE
-		/// </summary>
-		/// <param name="numbers">The values' list.</param>
-		/// <returns>The only not-repeated value.</returns>
-		private int FoundSingleValue2(List<int> numbers)
-		{
-			List<int> nums = new List<int>();
-			HashSet<int> set = new HashSet<int>();
-			foreach(int n in numbers)
+			foreach(int num in numbers)
 			{
-				if (set.Add(n))
-					nums.Add(n);
-				else
-					nums.Remove(n);
+				twos = twos | (ones & num);
+				ones = ones ^ num;
+				common_bits = ~(ones & twos);
+				ones &= common_bits;
+				twos &= common_bits;
 			}
-			return nums[0];
+
+			return ones;
 		}
 	}
 }

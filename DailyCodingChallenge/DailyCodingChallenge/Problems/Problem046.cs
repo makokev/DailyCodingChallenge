454b6d7ac9651c8151b9059dd1974b9854848a69
Kevin Leto <kevinleto1995@gmail.com>
2019-02-16 14:48:07 +0100

    Problem #46 and #47 solved.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem046.cs
@@ -0,0 +1,41 @@
+﻿using DailyCodingChallenge.Problems.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem046 : Problem
+	{
+		protected override string Description => ProblemDescription.PROBLEM046_DESCRIPTION;
+		protected override int Number => 46;
+
+		protected override void Run()
+		{
+			Console.Write("Insert the string: ");
+			string inputString = Console.ReadLine().Trim();
+
+			bool found = false;
+			string palindromicSubstring = "";
+			
+			for(int i = inputString.Length; i > 0 && !found; i--)
+			{
+				foreach(string sub in inputString.GetAllSubstrings(i))
+				{
+					if(sub.IsPalindrome())
+					{
+						palindromicSubstring = sub;
+						found = true;
+						break;
+					}
+				}
+			}
+			if (found)
+				Console.WriteLine("Max palindromic substring of '{0}' is '{1}' (of length {2}).", inputString, palindromicSubstring, palindromicSubstring.Length);
+			else
+				Console.WriteLine("No palindromic substring founded.");
+		}
+	}
+}
---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem047.cs
@@ -0,0 +1,48 @@
+﻿using DailyCodingChallenge.Problems.Utility;
+using DailyCodingChallenge.Problems.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem047 : Problem
+	{
+		protected override string Description => ProblemDescription.PROBLEM047_DESCRIPTION;
+		protected override int Number => 47;
+
+		protected override void Run()
+		{
+			int[] prices = new int[] { 9, 11, 8, 5, 7, 10 };
+
+			Console.WriteLine("Prices: {0}.", prices.Print());
+
+			Couple<int> couple = new Couple<int>(int.MaxValue, int.MinValue);
+			int profit = couple.Second - couple.First;
+			for(int i = 0; i < prices.Length-1; i++)
+			{
+				int currentMax = GetMaxFromIndex(prices, i);
+				int currentProfit = currentMax - profit;
+				if(profit < currentProfit)
+				{
+					couple = new Couple<int>(prices[i], currentMax);
+					profit = couple.Second - couple.First;
+				}
+			}
+
+			Console.WriteLine("Buying at {0}E and selling at {1}E you can earn a max profit of {2}E.", couple.First, couple.Second, profit);
+		}
+
+		private int GetMaxFromIndex(int[] values, int index)
+		{
+			if (index > values.Length)
+				throw new ArgumentOutOfRangeException("index", "0 <= index < values.Length");
+			int max = int.MinValue;
+			for(int i = index; i < values.Length; i++)
+				max = (values[i] > max) ? values[i] : max;
+			return max;
+		}
+	}
+}

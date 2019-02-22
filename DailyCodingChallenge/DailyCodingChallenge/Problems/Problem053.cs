248ebf3ac497d6aed4ffed7035e74a32d4971388
Kevin Leto <kevinleto1995@gmail.com>
2019-02-22 18:34:21 +0100

    Problem #53 solved.

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem053.cs
@@ -0,0 +1,37 @@
+﻿using DailyCodingChallenge.Utility;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem053 : Problem
+	{
+		protected override string Description => ProblemDescription.PROBLEM053_DESCRIPTION;
+		protected override int Number => 53;
+
+		protected override void Run()
+		{
+			int num = 3;
+			StackedQueue<int> queue = new StackedQueue<int>();
+
+			// filling the queue
+			Console.WriteLine("Queueing values:");
+			for (int i = 0; i < num; i++)
+			{
+				Console.WriteLine("Queued value '{0}'.", i);
+				queue.Enque(i);
+			}
+
+			// remove all values and check the correct extraction order
+			int value;
+			Console.WriteLine("\nDequeueing values:");
+			for(int i = 0; i < num; i++)
+			{
+				value = queue.Dequeue();
+				Console.WriteLine("Dequeued correct value '{0}': {1}.", value, value == i);
+			}
+		}
+	}
+}
---
+++ DailyCodingChallenge/DailyCodingChallenge/Utility/StackedQueue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Utility
+{
+	class StackedQueue<ValueType>
+	{
+		private readonly Stack<ValueType> _stack1;
+		private readonly Stack<ValueType> _stack2;
+
+		private Stack<ValueType> Stack1 { get => _stack1; }
+		private Stack<ValueType> Stack2 { get => _stack2; }
+		private bool Stack1Used { get; set; }
+
+		public int Count { get => (Stack1Used) ? Stack1.Count : Stack2.Count; }
+
+		public StackedQueue()
+		{
+			Stack1Used = true;
+			_stack1 = new Stack<ValueType>();
+			_stack2 = new Stack<ValueType>();
+		}
+
+		public void Enque(ValueType value)
+		{
+			if (!Stack1Used)
+			{
+				while (Stack2.Count > 0)
+				{
+					Stack1.Push(Stack2.Pop());
+				}
+				Stack1Used = true;
+			}
+			Stack1.Push(value);
+		}
+
+		public ValueType Dequeue()
+		{
+			if (Stack1Used)
+			{
+				Revert(Stack1, Stack2);
+				while(Stack1.Count > 0)
+				{
+					Stack2.Push(Stack1.Pop());
+				}
+				Stack1Used = false;
+			}
+			return (Stack2.Count > 0) ? Stack2.Pop() : default(ValueType);
+		}
+
+		private void Revert(Stack<ValueType> from, Stack<ValueType> to)
+		{
+			while (from.Count > 0)
+			{
+				to.Push(from.Pop());
+			}
+		}
+	}
+}

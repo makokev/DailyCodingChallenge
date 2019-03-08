22416b34f2848943bce1d4e121d3eaa93eeb4d84
Kevin Leto <kevinleto1995@gmail.com>
2019-03-08 11:42:23 +0100

    Problem #55 not implemented yet (Url Shortner).

---
+++ DailyCodingChallenge/DailyCodingChallenge/Problems/Problem055.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingChallenge.Problems
+{
+	class Problem055 : Problem
+	{
+		protected override string Description => ProblemDescription.PROBLEM055_DESCRIPTION;
+		protected override int Number => 55;
+
+		protected override void Run()
+		{
+			Console.WriteLine("Implementation not available jet.");
+		}
+	}
+}
